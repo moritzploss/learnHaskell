f04b395f6a58308cb06a9f6b779b4e3afbab7c8d
Moritz <moritz.ploss@gmail.com>
2020-06-20 19:50:58 +0200

    introduce submodules for tests

---
+++ test/Spec/Take.hs
@@ -0,0 +1,40 @@
+module Spec.Take where
+
+import Test.HUnit
+import qualified Lib
+
+testTakeOne :: Test
+testTakeOne =
+  TestCase $ assertEqual
+    "Should take first element from list"
+    (take 1 [1, 2, 3])
+    (Lib.take 1 [1, 2, 3])
+
+testTakeThree :: Test
+testTakeThree =
+  TestCase $ assertEqual
+    "Should take 3 first elements from list"
+    (take 3 [1, 2, 3, 4, 5])
+    (Lib.take 3 [1, 2, 3, 4, 5])
+
+testTakeZero :: Test
+testTakeZero =
+  TestCase $ assertEqual
+  "Should return empty list"
+  []
+  (Lib.take 0 [1, 2, 3])
+  
+testTakeExcess :: Test
+testTakeExcess =
+  TestCase $ assertEqual
+  "Should return list if n greater list length"
+  (take 5 [1, 2, 3])
+  (Lib.take 5 [1, 2, 3])
+
+testList :: Test
+testList = TestList [
+     testTakeOne
+   , testTakeThree
+   , testTakeZero
+   , testTakeExcess
+   ]
