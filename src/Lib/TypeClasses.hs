408bb83ef371d3e37eb47e49123b1e7b25e3a6c3
Moritz <moritz.ploss@gmail.com>
2020-07-24 21:05:29 +0200

    exercise types and value constructors

--- src/Lib/TypeClasses.hs
+++ src/Lib/TypeClasses.hs
@@ -80,3 +80,18 @@ instance Fnctor Maybe where
 instance Fnctor (Either a) where
   fmp func (Left x)  = Left x
   fmp func (Right y) = Right $ func y
+
+data Point = Point Float Float deriving (Show)
+
+data Shape = 
+  Circle Point Float |
+  Rectangle Point Point
+  deriving (Show)
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = abs $ (x1 - x2) * (y1 - y2)
+
+translate :: Shape -> Float -> Float -> Shape
+translate (Circle (Point x y) r) dx dy = Circle (Point (x + dx) (y + dy)) r
+translate (Rectangle (Point x1 y1) (Point x2 y2)) dx dy = Rectangle (Point (x1 + dx) (y1 + dy)) (Point (x2 + dx) (y2 + dy))
