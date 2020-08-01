cfa805d303eeed20e194ed9ce6df0b8a58c81c24
moritzploss <moritz.ploss@gmail.com>
2020-08-01 19:47:24 +0200

    refactor MergeChecker

--- src/Katas/MergeChecker.hs
+++ src/Katas/MergeChecker.hs
@@ -1,23 +1,26 @@
-module Katas.MergeChecker (isMerge) where
+--At a job interview, you are challenged to write an algorithm to check if a
+--given string, s, can be formed from two other strings, part1 and part2.
+--
+--The restriction is that the characters in part1 and part2 should be in the
+--same order as in s.
+--
+--The interviewer gives you the following example and tells you to figure out
+--the rest from the given test cases.
+--
+--For example:
+--
+--'codewars' is a merge from 'cdw' and 'oears':
+--
+--    s:  c o d e w a r s   = codewars
+--part1:  c   d   w         = cdw
+--part2:    o   e   a r s   = oears
 
-matchLength :: Eq a => [a] -> [a] -> Int
-matchLength [] _ = 0
-matchLength _ [] = 0
-matchLength (x:list) (y:otherList)
-  | x == y    = 1 + matchLength list otherList
-  | otherwise = 0
+module Katas.MergeChecker (isMerge) where
 
 isMerge :: String -> String -> String -> Bool
 isMerge x  y  [] = x == y
 isMerge x  []  z = x == z
 isMerge [] _   _ = False
-isMerge (x:xs) partY@(y:ys) partZ@(z:zs)
-  | (x /= y) && (x /= z) = False
-  | (x == y) && (x /= z) = isMerge xs ys partZ
-  | (x /= y) && (x == z) = isMerge xs partY zs
-  | otherwise            =
-    if matchLenY > matchLenZ
-    then isMerge xs ys partZ
-    else isMerge xs partY zs
-    where matchLenY = matchLength xs ys
-          matchLenZ = matchLength xs zs
+isMerge (x:xs) part1@(y:ys) part2@(z:zs) =
+  x == y && isMerge xs ys part2
+         || isMerge xs part1 zs
