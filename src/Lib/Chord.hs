f552b1bedca4e2b326ae16eea851a6b629974ad6
moritzploss <moritz.ploss@gmail.com>
2020-08-20 20:58:04 +0200

    Add minor chord composition functions

--- src/Lib/Chord.hs
+++ src/Lib/Chord.hs
@@ -1,14 +1,37 @@
 module Lib.Chord
   ( Chord (..)
   , transpose
+  , minor
   ) where
 
 import qualified Lib.Note as Note
+import qualified Data.IntSet as IntSet
+
+type PitchClass = Int
 
 data Chord = Chord
-  { pitchClass :: Int
-  , notes :: [Int] } deriving (Show, Eq)
+  { pitchClass :: PitchClass
+  , notes :: IntSet.IntSet
+  } deriving (Show, Eq)
 
 transpose :: Int -> Chord -> Chord
 transpose step chord = chord { pitchClass = transposed }
   where transposed = Note.transpose step $ pitchClass chord
+
+add :: Int -> Chord -> Chord
+add n chord = chord { notes = IntSet.insert n $ notes chord }
+
+addRoot :: Chord -> Chord
+addRoot = add 0
+
+addMinorThird :: Chord -> Chord
+addMinorThird = add 3
+
+addPerfectFifth :: Chord -> Chord
+addPerfectFifth = add 7
+
+base :: PitchClass -> Chord
+base pitchClass = Chord pitchClass IntSet.empty
+
+minor :: PitchClass -> Chord
+minor = addRoot . addMinorThird . addPerfectFifth . base
--- test/Spec/Chord.hs
+++ test/Spec/Chord.hs
@@ -1,6 +1,7 @@
 module Spec.Chord (spec) where
 
-import Lib.Chord (Chord (Chord), transpose)
+import Lib.Chord (Chord (Chord), transpose, minor)
+import Data.IntSet (fromList)
 
 import Control.Applicative
 import Test.Hspec
@@ -11,10 +12,19 @@ spec = do
     describe "Chords" $ do
         describe "transpose" $ do
             it "positive without wrapping" $
-                transpose 2 (Chord 0 [1, 2, 3]) `shouldBe` Chord 2 [1, 2, 3]
+                transpose 2 (Chord 0 notes) `shouldBe` Chord 2 notes
             it "positive with wrapping" $
-                transpose 14 (Chord 0 [1, 2, 3]) `shouldBe` Chord 2 [1, 2, 3]
+                transpose 14 (Chord 0 notes) `shouldBe` Chord 2 notes
             it "negative without wrapping" $
-                transpose (-3) (Chord 5 [1, 2, 3]) `shouldBe` Chord 2 [1, 2, 3]
+                transpose (-3) (Chord 5 notes) `shouldBe` Chord 2 notes
             it "negative with wrapping" $
-                transpose (-15) (Chord 5 [1, 2, 3]) `shouldBe` Chord 2 [1, 2, 3]
+                transpose (-15) (Chord 5 notes) `shouldBe` Chord 2 notes
+
+        describe "chord composition" $ do
+            it "compose minor chord" $
+                minor 2 `shouldBe` Chord 2 minorNotes
+
+    where
+        notes      = fromList [1, 2, 3]
+        minorNotes = fromList [0, 3, 7]
+
